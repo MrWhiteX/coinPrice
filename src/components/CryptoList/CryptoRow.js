970689d0251d905681c493735064b1b6413c2fd7
MrWhiteX <bartlomiej.bankosz@gmail.com>
2021-07-08 21:33:39 +0200

    favourite and localstorage

--- src/components/CryptoList/CryptoRow.js
+++ src/components/CryptoList/CryptoRow.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const CryptoRow = (props) => {
   const { crypto } = props;
@@ -6,6 +8,11 @@ const CryptoRow = (props) => {
   const sortedCrypto = crypto.sort(function (a, b) {
     return a.rank - b.rank;
   });
+
+  const handleFavouriteClick = (currency) => {
+    console.log(currency);
+    localStorage.setItem(`${currency.symbol}`, JSON.stringify(currency));
+  };
 
   return (
     <>
@@ -19,7 +26,14 @@ const CryptoRow = (props) => {
             <td>{currency.quotes.USD.price}</td>
             <td>{currency.quotes.USD.percent_change_24h}</td>
             <td>{currency.quotes.USD.percent_change_7d}</td>
-            <td>STAR</td>
+            <td>
+              <FontAwesomeIcon
+                className="text-danger"
+                icon={faStar}
+                style={{ cursor: "pointer" }}
+                onClick={() => handleFavouriteClick(currency)}
+              />
+            </td>
           </tr>
         );
       })}
